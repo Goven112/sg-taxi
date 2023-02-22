2a98318cc4a58f2fbc53be05d09476bd5cd4f4d9
Goven112 <60715039+Goven112@users.noreply.github.com>
2023-02-22 17:04:20 +0200

    change adress server

--- src/utilities/Constrans.ts
+++ src/utilities/Constrans.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL_DEVELOPMENT: string  = 'https://localhost:44350';
+const API_BASE_URL_DEVELOPMENT: string  = 'https://taxiserver.azurewebsites.net/';
 const API_BASE_URL_PRODUCTION :string = 'https://aspnetcorereacttutorial-aspnetserver.azurewebsites.net';
 
 
